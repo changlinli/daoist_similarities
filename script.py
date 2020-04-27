bb74e2b64623ec6e2a02e079e3efd623c4e7db91
Changlin Li <mail@changlinli.com>
2020-04-27 01:59:53 -0400

    Add similarity scores

--- script.py
+++ script.py
@@ -1,4 +1,8 @@
 import csv
+import glob
+import math
+
+from itertools import islice
 
 filename = "data/daoist/Huainanzi_淮南子_chu_zhen_xun_俶真訓.txt.json.csv"
 
@@ -9,7 +13,10 @@ def retrieve_characters_to_frequency(input_file):
     with open(input_file, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         # Drop the first row which is just column headers
-        next(reader)
+        try:
+            next(reader)
+        except StopIteration:
+            print("This file was empty: " + input_file)
         for row in reader:
             character = row[0]
             absolute_frequency = row[1]
@@ -39,33 +46,167 @@ def normalize_as_term_frequencies(characters_to_frequency):
                              characters_to_frequency.items()}
     return term_frequency_scores
 
-print(normalize_as_term_frequencies(retrieve_characters_to_frequency(filename)))
+def retrieve_term_frequences_from_file(input_file):
+    return normalize_as_term_frequencies(retrieve_characters_to_frequency(input_file))
 
+daoist_texts = glob.glob("data/daoist/*.json.csv")
 
-#print(term_frequency_scores)
+general_texts = glob.glob("data/general/*.json.csv")
 
-distinct_characters = []
+daoist_text_term_frequencies = {}
 
-distinct_characters = distinct_characters + list(characters_to_frequency.keys())
+for file_name in daoist_texts:
+    daoist_text_term_frequencies[file_name] = retrieve_term_frequences_from_file(file_name)
 
-documents_to_words = {}
+general_text_term_frequencies = {}
 
-documents_to_words.update({filename: distinct_characters})
+for file_name in general_texts:
+    general_text_term_frequencies[file_name] = retrieve_term_frequences_from_file(file_name)
 
+all_term_frequencies = {**daoist_text_term_frequencies,
+                        **general_text_term_frequencies}
 
-words_to_documents = {}
+#print(all_term_frequencies)
 
-for document, words in documents_to_words.items():
-    for word in words:
-        currently_put = words_to_documents.get(word, [])
+documents_to_distinct_characters = {}
+
+for document, term_frequencies in all_term_frequencies.items():
+    distinct_characters = []
+    distinct_characters = distinct_characters + list(term_frequencies.keys())
+    documents_to_distinct_characters[document] = distinct_characters
+
+#print(documents_to_distinct_characters)
+
+characters_to_documents = {}
+
+for document, characters in documents_to_distinct_characters.items():
+    for character in characters:
+        currently_put = characters_to_documents.get(character, [])
         currently_put.append(document)
-        words_to_documents[word] = currently_put
+        characters_to_documents[character] = currently_put
 
+#print(characters_to_documents)
 
 document_frequency_scores = {}
 
-for word, document in words_to_documents.items():
-    document_frequency_scores[word] = len(document)
+for character, documents in characters_to_documents.items():
+    document_frequency_scores[character] = len(documents)
+
+#print(document_frequency_scores)
+
+number_of_documents_in_corpus = len(general_texts + daoist_texts)
+
+inverse_document_frequency_scores = {}
+
+for character, frequency_score in document_frequency_scores.items():
+    inverse_document_frequency_scores[character] = \
+        math.log(number_of_documents_in_corpus / (frequency_score + 1))
+
+#print(inverse_document_frequency_scores)
+
+tf_idf_scores = {}
+
+for document, term_frequencies in all_term_frequencies.items():
+    tf_idf_for_document = {}
+    for character, frequency in term_frequencies.items():
+        idf = inverse_document_frequency_scores[character]
+        tf_idf_for_document[character] = frequency * idf
+    tf_idf_scores[document] = tf_idf_for_document
+
+#print(tf_idf_scores)
+
+def dot_product_between_two_term_frequencies(term_frequencies_0,
+                                             term_frequencies_1):
+    current_sum = 0
+    for character, term_frequency in term_frequencies_0.items():
+        other = term_frequencies_1.get(character, 0)
+        current_sum = current_sum + term_frequency * other
+    return current_sum
+
+def magnitude(term_frequencies):
+    current_sum = 0
+    for character, term_frequency in term_frequencies.items():
+        current_sum = current_sum + (term_frequency ** 2)
+    return math.sqrt(current_sum)
+
+def similarity_between_two_documents(tf_idf_0, tf_idf_1):
+    numerator = dot_product_between_two_term_frequencies(tf_idf_0, tf_idf_1)
+    denominator = magnitude(tf_idf_0) * magnitude(tf_idf_1)
+    if denominator == 0:
+        return 0
+    else:
+        return numerator / denominator
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
+
+#######
+# Huainanzi
+#######
+
+default_tf_idf_score = tf_idf_scores[filename]
+
+similarities_to_default = {}
+
+for document, tf_idf_score in tf_idf_scores.items():
+    similarities_to_default[document] = \
+        similarity_between_two_documents(default_tf_idf_score, tf_idf_score)
+
+sorted_similarities_to_huainanzi = {k: v for k, v in sorted(similarities_to_default.items(), key=lambda item: item[1])}
+
+top_25_huainanzi = take(25, reversed(sorted_similarities_to_huainanzi.items()))
+
+print("Top 25 most similar to Huainanzi:")
+
+for filename, similarity_score in top_25_huainanzi:
+    print(filename + ": " + repr(similarity_score))
+
+#######
+# Guanzi
+#######
+
+guanzi_file_name = "data/daoist/Guanzi_管子_Neiye_內業.txt.json.csv"
+
+guanzi_tf_idf_score = tf_idf_scores[guanzi_file_name]
+
+similarities_to_guanzi = {}
+
+for document, tf_idf_score in tf_idf_scores.items():
+    similarities_to_guanzi[document] = \
+        similarity_between_two_documents(guanzi_tf_idf_score, tf_idf_score)
+
+sorted_similarities_to_guanzi = {k: v for k, v in sorted(similarities_to_guanzi.items(), key=lambda item: item[1])}
+
+top_25_guanzi = take(25, reversed(sorted_similarities_to_guanzi.items()))
+
+print("Top 25 most similar to Guanzi:")
+
+for filename, similarity_score in top_25_guanzi:
+    print(filename + ": " + repr(similarity_score))
+
+#distinct_characters = []
+
+#distinct_characters = distinct_characters + list(characters_to_frequency.keys())
+
+#documents_to_words = {}
+
+#documents_to_words.update({filename: distinct_characters})
+
+
+#words_to_documents = {}
+
+#for document, words in documents_to_words.items():
+    #for word in words:
+        #currently_put = words_to_documents.get(word, [])
+        #currently_put.append(document)
+        #words_to_documents[word] = currently_put
+
+
+#document_frequency_scores = {}
+
+#for word, document in words_to_documents.items():
+    #document_frequency_scores[word] = len(document)
 
 #print(document_frequency_scores)
 
